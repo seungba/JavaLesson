4e78d5d0c28e04a4855a65defd8cc6b3c6fc2d27
seungba <dltmdwns0522@gmail.com>
2019-05-20 17:13:09 +0900

    TODO list

--- New/src/lesson20190509/ClientSocketEx.java
+++ New/src/lesson20190509/ClientSocketEx.java
@@ -18,28 +18,38 @@ public class ClientSocketEx {
 		ObjectOutputStream oos = null;
 		ObjectInputStream ois = null;
 		Scanner sc = new Scanner(System.in);
-		while(true) {
-			// establish socket connection to server
-//			socket = new Socket(host.getHostName(), 80);
+		System.out.println("3 " + socket);
+		
+		// establish socket connection to server
+//		socket = new Socket(host.getHostName(), 80);
+		System.out.println("4 " + socket);
+		socket = new Socket("localhost", 8080); // 여기서 소켓 포트가 바뀜
+		// write to socket using ObjectOutputStream
+
+		while (true) {
 			
-			socket = new Socket("localhost", 8080);
-			// write to socket using ObjectOutputStream
-			
+			System.out.println("5 " + socket);
 			oos = new ObjectOutputStream(socket.getOutputStream());
 			System.out.println("Sending request to Socket Server");
-			
+
 			System.out.print("입력하세요 : ");
 			String sendMessage = sc.nextLine();
 			oos.writeObject(sendMessage);
-			
+
 			// read the server response message
 			ois = new ObjectInputStream(socket.getInputStream());
 			String message = (String) ois.readObject();
-			System.out.println("Message: " + message);
-			// close resources
-			ois.close();
-			oos.close();
-			Thread.sleep(100);
+			System.out.println("Message: " + message); // 서버에서 받은 메시지
+
+			if (message == "exit") {
+				break;
+			}
 		}
+
+		// close resources
+		ois.close();
+		oos.close();
+		Thread.sleep(100);
+		System.out.println("6 " + socket);
 	}
 }
--- New/src/lesson20190509/ServerSocketEx.java
+++ New/src/lesson20190509/ServerSocketEx.java
@@ -15,49 +15,83 @@ public class ServerSocketEx {
 	private static int port = 8080;
 
 	public static void main(String args[]) throws IOException, ClassNotFoundException {
-		
+
 		// create the socket server object
 		server = new ServerSocket(port);
-		
 
 		Socket socket = null;
 		ObjectOutputStream oos = null;
 		ObjectInputStream ois = null;
-		
+
 		ArrayList<Socket> list = new ArrayList<>();
+
+
+		System.out.println("Waiting for the client request");
+		// creating socket and waiting for client connection
 		
 		// keep listens indefinitely until receives 'exit' call or program terminates
 		while (true) {
-			System.out.println("Waiting for the client request");
-			// creating socket and waiting for client connection
+			System.out.println("2 socket " + socket);
+			System.out.println("2 list " + list);
+			
 			socket = server.accept(); // 대기 중...
+			
+			System.out.println("3 socket " + socket);
+			System.out.println("3 list " + list);
+			
 			list.add(socket);
 
 			// TODO list에 있는 모든 socket한테 메시지를 전달!!!!!
+
+			System.out.println("4 socket " + socket);
+			System.out.println("4 list " + list);
 			
-			System.out.println(list);
-			
-			
-			// read from socket to ObjectInputStream object
-			ois = new ObjectInputStream(socket.getInputStream());
-			// convert ObjectInputStream object to String
-			String message = (String) ois.readObject();
-			System.out.println("Message Received: " + message);
-			// create ObjectOutputStream object
-			oos = new ObjectOutputStream(socket.getOutputStream());
-			// write object to Socket
-			oos.writeObject("Hi Client " + message);
-			// close resources
-			
-			// terminate the server if client sends exit request
-			if (message.equalsIgnoreCase("exit"))
-				break;
+			for(int i = 0; i < list.size(); i++){
+				System.out.println("5 socket " + socket);
+				System.out.println("5 list " + list);
+				System.out.println(list.get(i));	
+				System.out.println("6 socket " + socket);
+				System.out.println("6 list " + list);
+				// read from socket to ObjectInputStream object
+//				ois = new ObjectInputStream(socket.getInputStream());
+				ois = new ObjectInputStream(list.get(i).getInputStream());
+				System.out.println("7 socket " + socket);
+				System.out.println("7 list " + list);
+				// convert ObjectInputStream object t o String
+				String message = (String) ois.readObject();
+				System.out.println("8 socket " + socket);
+				System.out.println("8 list " + list);
+				System.out.println("Message Received: " + message);
+				System.out.println("9 socket " + socket);
+				System.out.println("9 list " + list);
+				// create ObjectOutputStream object
+//				oos = new ObjectOutputStream(socket.getOutputStream());
+				oos = new ObjectOutputStream(list.get(i).getOutputStream());
+				// write object to Socket
+				System.out.println("10 socket " + socket);
+				System.out.println("10 list " + list);
+				oos.writeObject("Hi Client " + message); // 클라이언트로 보내는 메시지
+				// close resources
+				
+				System.out.println("11 socket " + socket);
+				System.out.println("11 list " + list);
+
+				// terminate the server if client sends exit request
+				if (message.equalsIgnoreCase("exit")) {
+					System.out.println("12 socket " + socket);
+					System.out.println("12 list " + list);
+					ois.close();
+					oos.close();
+					socket.close();
+					System.out.println("13 socket " + socket);
+					System.out.println("13 list " + list);
+					System.out.println("Shutting down Socket server!!");
+					// close the ServerSocket object
+					server.close();
+					System.out.println("14 socket " + socket);
+					System.out.println("14 list " + list);
+				}
+			}
 		}
-		ois.close();
-		oos.close();
-		socket.close();
-		System.out.println("Shutting down Socket server!!");
-		// close the ServerSocket object
-		server.close();
 	}
 }
